e7a7147962db423138d2bb060f788403c2fbd826
Joel-Alves-Santos <joel.alves.santos77@gmail.com>
2023-06-13 15:16:56 -0300

    commit

---
+++ Pasta Geral/Listas das Aulas e Dojôs/Aulas/Portifolios/Selection_Sort.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//Exemplo de Selection Sort
+
+int main(void)
+{
+    //Declarando vetor e atribuindo valores
+    int vetor[] = {12,44,90,129,23,5,11};
+
+    
+
+    return 0;
+}
